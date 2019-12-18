cbf5ae1aa9663b957bf00a1318106ea20f5f5a53
Panfi Tshimanga <panfitshims@live.com>
2019-12-18 10:47:27 +0200

    Single pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import ServicePage from './servicePage';
 import ContactPage from './contactPage';
 import BlogPage from './blogPage ';
 import SingleBlogPage from './singleBlogPage';
+import PenTest from './pentestPage';
+import RedTeam from './redteamPage';
+import CyberDef from './cyberdefPage';
 
-
-export { Home, ServicePage, ContactPage, BlogPage, SingleBlogPage };
+export { Home, ServicePage, ContactPage, BlogPage, SingleBlogPage,PenTest, RedTeam, CyberDef};
