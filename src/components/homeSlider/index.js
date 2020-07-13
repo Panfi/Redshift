58364470d2ebf321cf7bee092dc56d0f6cf727e4
ozvadez <oswald@flume.co.za>
2020-07-13 11:46:27 +0200

    homeSlider component
    Added a new component, a bootstrap slider for the homepage "Our three step cyber approach"

---
+++ src/components/homeSlider/index.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import sliderHeader from '../../assets/images/slider-top1.jpg';
+import sliderBtn from '../../assets/images/slider-btn.fw.png';
+import './slider.css'
+import Carousel from 'react-bootstrap/Carousel'  
+
+
+export class BootstrapCarousel extends Component {  
+
+    render() {  
+        return (  
+            <div>
+               
+                <div className='container-fluid' >
+                    <Carousel autoPlay={true}
+                    interval={5000}
+                    controls={false}
+                    indicators={true}>
+                        <Carousel.Item style={{'height':"460px"}} >
+                            <img style={{'height':"40%"}} className="d-block w-100" src={sliderHeader} />
+                            <Carousel.Caption>
+                                <span class="c-text-line"></span>
+                                <p class="c-text">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </p>
+                                <div class="slider-btn-cell">
+                                    <a href="#"  class="slider-btn">
+                                        <img src={sliderBtn} />
+                                    </a>
+                                </div>
+                            </Carousel.Caption>
+                        </Carousel.Item  >
+
+                        <Carousel.Item style={{'height':"460px"}} >
+                            <img style={{'height':"40%"}} className="d-block w-100" src={sliderHeader} />
+                            <Carousel.Caption>
+                                <span class="c-text-line"></span>
+                                <p class="c-text">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </p>
+                                <div class="slider-btn-cell">
+                                    <a href="#"  class="slider-btn">
+                                        <img src={sliderBtn} />
+                                    </a>
+                                </div>
+                            </Carousel.Caption>
+                        </Carousel.Item  >
+
+                        <Carousel.Item style={{'height':"460px"}} >
+                            <img style={{'height':"40%"}} className="d-block w-100" src={sliderHeader} />
+                            <Carousel.Caption>
+                                <span class="c-text-line"></span>
+                                <p class="c-text">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </p>
+                                <div class="slider-btn-cell">
+                                    <a href="#"  class="slider-btn">
+                                        <img src={sliderBtn} />
+                                    </a>
+                                </div>
+                            </Carousel.Caption>
+                        </Carousel.Item  >
+
+                       
+                           
+                    </Carousel>
+                </div>
+            </div>
+        )
+    }
+}  
+
+export default BootstrapCarousel  
